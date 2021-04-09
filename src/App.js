5bec9f6494480c40cd6a658819a3931d0d8fb57f
Sarah Deitke <sarah@Sarahs-MBP.attlocal.net>
2021-04-08 21:01:08 -0400

    selection layout

--- src/App.js
+++ src/App.js
@@ -7,9 +7,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Box office is a monthly subscription box service from N*tflix bringing everything you love about your favorite shows into your home. Build your box below!
+          Box office is a monthly subscription box service from Netfl*x bringing everything you love about your favorite shows into your home. Build your box below!
         </p>
       </header>
+
+      <main className="App-main">
+        <p>1. Select Show</p>
+        <p>2. Select Small Merch</p>
+        <p>3. Select Big Merch</p>
+        <p>4. Select Snack Sweet</p>
+        <p>5. Select Snack Salty</p>
+        <button>pre-order May</button>
+      </main>
     </div>
   );
 }
