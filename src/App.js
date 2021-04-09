78344812c948f08a2abb009b646a9d440964d968
Sarah Deitke <sarah@Sarahs-MBP.attlocal.net>
2021-04-08 20:48:48 -0400

    header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Box office is a monthly subscription box service from N*tflix bringing everything you love about your favorite shows into your home. Build your box below!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
