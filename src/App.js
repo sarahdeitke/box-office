8d1bae60b519f11897559dab272c28fbd14bcbea
Sarah Deitke <sarah@Sarahs-MBP.attlocal.net>
2021-04-14 12:15:22 -0400

    back end google sheets

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './Images/logo.png';
 import { Container, Row,Col } from 'reactstrap';
 import TagManager from 'react-gtm-module';
@@ -29,8 +31,37 @@ const tagManagerArgs = {
 
 TagManager.initialize(tagManagerArgs)
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: '',
+      show: '',
+      bigM: '',
+      smallM: '',
+      sweetS: '',
+      saltyS: ''
+    };
+  }
+
+  handleOptionChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  submitHandler = e => {
+    e.preventDefault();
+    console.log(this.state);
+  
+    axios.post('https://sheet.best/api/sheets/ddafecbb-a2be-4dd9-a201-31f3ec160f8d', this.state)
+    .then(response => {
+      console.log(response);
+    })
+  }
+
+  render() {
+    return(
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -42,43 +73,48 @@ function App() {
       <Container fluid className="App-main">
         <p>1. Select May Show</p>
         <Row className="Selection-row">
-          <Col><label><input type="radio" name="show" id="ST"/><img className="Selection-box" src={StrangerThings}/></label></Col>
-          <Col><label><input type="radio" name="show" id="MH"/><img className="Selection-box" src={MoneyHeist}/></label></Col>
-          <Col><label><input type="radio" name="show" id="Br"/><img className="Selection-box" src={Bridgerton}/></label></Col>
-          <Col><label><input type="radio" name="show" id="Av"/><img className="Selection-box" src={Avatar}/></label></Col>
+          <Col><label><input type="radio" name="show" value="ST" onChange={this.handleOptionChange}/><img className="Selection-box" src={StrangerThings}/></label></Col>
+          <Col><label><input type="radio" name="show" value="MH" onChange={this.handleOptionChange}/><img className="Selection-box" src={MoneyHeist}/></label></Col>
+          <Col><label><input type="radio" name="show" value="Br" onChange={this.handleOptionChange}/><img className="Selection-box" src={Bridgerton}/></label></Col>
+          <Col><label><input type="radio" name="show" value="Av" onChange={this.handleOptionChange}/><img className="Selection-box" src={Avatar}/></label></Col>
         </Row>
         <p>2. Select Branded Small Merch</p>
         <Row className="Selection-row">
-          <Col><label><input type="radio" name="smallM" id="so"/><img className="Selection-box" src={CrewSocks}/></label></Col>
-          <Col><label><input type="radio" name="smallM" id="be"/><img className="Selection-box" src={Beanie}/></label></Col>
-          <Col><label><input type="radio" name="smallM" id="st"/><img className="Selection-box" src={Stickers}/></label></Col>
-          <Col><label><input type="radio" name="smallM" id="mu"/><img className="Selection-box" src={Mug}/></label></Col>
+          <Col><label><input type="radio" name="smallM" value="so" onChange={this.handleOptionChange}/><img className="Selection-box" src={CrewSocks}/></label></Col>
+          <Col><label><input type="radio" name="smallM" value="be" onChange={this.handleOptionChange}/><img className="Selection-box" src={Beanie}/></label></Col>
+          <Col><label><input type="radio" name="smallM" value="st" onChange={this.handleOptionChange}/><img className="Selection-box" src={Stickers}/></label></Col>
+          <Col><label><input type="radio" name="smallM" value="mu" onChange={this.handleOptionChange}/><img className="Selection-box" src={Mug}/></label></Col>
         </Row>
         <p>3. Select Branded Big Merch</p>
         <Row className="Selection-row">
-          <Col><label><input type="radio" name="bigM" id="sh"/><img className="Selection-box" src={Shirt}/></label></Col>
-          <Col><label><input type="radio" name="bigM" id="hy"/><img className="Selection-box" src={Hydroflask}/></label></Col>
-          <Col><label><input type="radio" name="bigM" id="pa"/><img className="Selection-box" src={Pants}/></label></Col>
-          <Col><label><input type="radio" name="bigM" id="su"/><img className="Selection-box" src={Surprise}/></label></Col>
+          <Col><label><input type="radio" name="bigM" value="sh" onChange={this.handleOptionChange}/><img className="Selection-box" src={Shirt}/></label></Col>
+          <Col><label><input type="radio" name="bigM" value="hy" onChange={this.handleOptionChange}/><img className="Selection-box" src={Hydroflask}/></label></Col>
+          <Col><label><input type="radio" name="bigM" value="pa" onChange={this.handleOptionChange}/><img className="Selection-box" src={Pants}/></label></Col>
+          <Col><label><input type="radio" name="bigM" value="su" onChange={this.handleOptionChange}/><img className="Selection-box" src={Surprise}/></label></Col>
         </Row>
         <p>4. Select Snack Sweet</p>
         <Row className="Selection-row">
-          <Col><label><input type="radio" name="sweetS" id="mm"/><img className="Selection-box" src={MMs}/></label></Col>
-          <Col><label><input type="radio" name="sweetS" id="spk"/><img className="Selection-box" src={SourPatchKids}/></label></Col>
-          <Col><label><input type="radio" name="sweetS" id="re"/><img className="Selection-box" src={Reeses}/></label></Col>
-          <Col><label><input type="radio" name="sweetS" id="tw"/><img className="Selection-box" src={Twizzlers}/></label></Col>
+          <Col><label><input type="radio" name="sweetS" value="mm" onChange={this.handleOptionChange}/><img className="Selection-box" src={MMs}/></label></Col>
+          <Col><label><input type="radio" name="sweetS" value="spk" onChange={this.handleOptionChange}/><img className="Selection-box" src={SourPatchKids}/></label></Col>
+          <Col><label><input type="radio" name="sweetS" value="re" onChange={this.handleOptionChange}/><img className="Selection-box" src={Reeses}/></label></Col>
+          <Col><label><input type="radio" name="sweetS" value="tw" onChange={this.handleOptionChange}/><img className="Selection-box" src={Twizzlers}/></label></Col>
         </Row>
         <p>5. Select Snack Salty</p>
         <Row className="Selection-row">
-          <Col><label><input type="radio" name="saltyS" id="po"/><img className="Selection-box" src={Popcorn}/></label></Col>
-          <Col><label><input type="radio" name="saltyS" id="pr"/><img className="Selection-box" src={Pretzels}/></label></Col>
-          <Col><label><input type="radio" name="saltyS" id="ch"/><img className="Selection-box" src={Chips}/></label></Col>
-          <Col><label><input type="radio" name="saltyS" id="pe"/><img className="Selection-box" src={Peanuts}/></label></Col>
+          <Col><label><input type="radio" name="saltyS" value="po" onChange={this.handleOptionChange}/><img className="Selection-box" src={Popcorn}/></label></Col>
+          <Col><label><input type="radio" name="saltyS" value="pr" onChange={this.handleOptionChange}/><img className="Selection-box" src={Pretzels}/></label></Col>
+          <Col><label><input type="radio" name="saltyS" value="ch" onChange={this.handleOptionChange}/><img className="Selection-box" src={Chips}/></label></Col>
+          <Col><label><input type="radio" name="saltyS" value="pe" onChange={this.handleOptionChange}/><img className="Selection-box" src={Peanuts}/></label></Col>
         </Row>
-        <p><button className="Order-button">Pre-Order May</button></p>
+        <p>6. Name</p>
+        <Row className="Selection-row">
+          <input type="text" name="name" value={this.state.name} onChange={this.handleOptionChange}/>
+        </Row>
+        <p><button className="Order-button" onClick={this.submitHandler}>Pre-Order May</button></p>
       </Container>
     </div>
   );
+}
 }
 
 export default App;
