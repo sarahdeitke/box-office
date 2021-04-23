4d4299ab7581f698541291954f0e9c07d16de517
Sarah Deitke <sarah@Sarahs-MacBook-Pro.local>
2021-04-23 14:35:33 -0400

    changed layout and buttons

--- src/App.js
+++ src/App.js
@@ -23,6 +23,19 @@ import Popcorn from './Images/Popcorn.webp';
 import Peanuts from './Images/Peanuts.jpg';
 import Chips from './Images/Chips.jpg';
 import Pretzels from './Images/Pretzels.jpeg';
+import StTop1 from './Images/st_top_1.jpeg';
+import StTop2 from './Images/st_top_2.png';
+import StTop3 from './Images/st_top_3.png';
+import StTop4 from './Images/st_top_4.jpeg';
+import StMug1 from './Images/st_mug_1.png';
+import StMug2 from './Images/st_mug_2.png';
+import StMug3 from './Images/st_mug_3.png';
+import StMug4 from './Images/st_mug_4.png';
+import StMisc1 from './Images/st_misc_1.png';
+import StMisc2 from './Images/st_misc_2.png';
+import StMisc3 from './Images/st_misc_3.png';
+import StMisc4 from './Images/st_misc_4.png';
+
 import './App.css';
 
 const tagManagerArgs = {
@@ -68,33 +81,33 @@ class App extends React.Component {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Box Office is a monthly subscription box service from Netfl*x bringing everything you love about your favorite shows into your home. Build your box below!
+          You've entered the Stranger Things Limited Edition Netflix Merchandise Shop. Season 4 is coming out in May. Preorder your limited edition merch now.
         </p>
       </header>
 
       <Container fluid className="App-main">
-        <p>1. Select May Show</p>
+        <p>Tops</p>
         <Row className="Selection-row">
-          <Col><label><input type="radio" name="show" value="ST" onChange={this.handleOptionChange}/><img className="Selection-box" src={StrangerThings}/></label></Col>
-          <Col><label><input type="radio" name="show" value="MH" onChange={this.handleOptionChange}/><img className="Selection-box" src={MoneyHeist}/></label></Col>
-          <Col><label><input type="radio" name="show" value="Br" onChange={this.handleOptionChange}/><img className="Selection-box" src={Bridgerton}/></label></Col>
-          <Col><label><input type="radio" name="show" value="Av" onChange={this.handleOptionChange}/><img className="Selection-box" src={Avatar}/></label></Col>
+          <Col><label><input type="checkbox" name="show" value="ST" onChange={this.handleOptionChange}/><img className="Selection-box" src={StTop1}/></label></Col>
+          <Col><label><input type="checkbox" name="show" value="MH" onChange={this.handleOptionChange}/><img className="Selection-box" src={StTop2}/></label></Col>
+          <Col><label><input type="checkbox" name="show" value="Br" onChange={this.handleOptionChange}/><img className="Selection-box" src={StTop3}/></label></Col>
+          <Col><label><input type="checkbox" name="show" value="Av" onChange={this.handleOptionChange}/><img className="Selection-box" src={StTop4}/></label></Col>
         </Row>
-        <p>2. Select Branded Small Merch</p>
+        <p>Mugs</p>
         <Row className="Selection-row">
-          <Col><label><input type="radio" name="smallM" value="so" onChange={this.handleOptionChange}/><img className="Selection-box" src={CrewSocks}/></label></Col>
-          <Col><label><input type="radio" name="smallM" value="be" onChange={this.handleOptionChange}/><img className="Selection-box" src={Beanie}/></label></Col>
-          <Col><label><input type="radio" name="smallM" value="st" onChange={this.handleOptionChange}/><img className="Selection-box" src={Stickers}/></label></Col>
-          <Col><label><input type="radio" name="smallM" value="mu" onChange={this.handleOptionChange}/><img className="Selection-box" src={Mug}/></label></Col>
+          <Col><label><input type="checkbox" name="smallM" value="so" onChange={this.handleOptionChange}/><img className="Selection-box" src={StMug1}/></label></Col>
+          <Col><label><input type="checkbox" name="smallM" value="be" onChange={this.handleOptionChange}/><img className="Selection-box" src={StMug2}/></label></Col>
+          <Col><label><input type="checkbox" name="smallM" value="st" onChange={this.handleOptionChange}/><img className="Selection-box" src={StMug3}/></label></Col>
+          <Col><label><input type="checkbox" name="smallM" value="mu" onChange={this.handleOptionChange}/><img className="Selection-box" src={StMug4}/></label></Col>
         </Row>
-        <p>3. Select Branded Big Merch</p>
+        <p>Misc.</p>
         <Row className="Selection-row">
-          <Col><label><input type="radio" name="bigM" value="sh" onChange={this.handleOptionChange}/><img className="Selection-box" src={Shirt}/></label></Col>
-          <Col><label><input type="radio" name="bigM" value="hy" onChange={this.handleOptionChange}/><img className="Selection-box" src={Hydroflask}/></label></Col>
-          <Col><label><input type="radio" name="bigM" value="pa" onChange={this.handleOptionChange}/><img className="Selection-box" src={Pants}/></label></Col>
-          <Col><label><input type="radio" name="bigM" value="su" onChange={this.handleOptionChange}/><img className="Selection-box" src={Surprise}/></label></Col>
+          <Col><label><input type="checkbox" name="bigM" value="sh" onChange={this.handleOptionChange}/><img className="Selection-box" src={StMisc1}/></label></Col>
+          <Col><label><input type="checkbox" name="bigM" value="hy" onChange={this.handleOptionChange}/><img className="Selection-box" src={StMisc2}/></label></Col>
+          <Col><label><input type="checkbox" name="bigM" value="pa" onChange={this.handleOptionChange}/><img className="Selection-box" src={StMisc3}/></label></Col>
+          <Col><label><input type="checkbox" name="bigM" value="su" onChange={this.handleOptionChange}/><img className="Selection-box" src={StMisc4}/></label></Col>
         </Row>
-        <p>4. Select Snack Sweet</p>
+        {/* <p>4. Select Snack Sweet</p>
         <Row className="Selection-row">
           <Col><label><input type="radio" name="sweetS" value="mm" onChange={this.handleOptionChange}/><img className="Selection-box" src={MMs}/></label></Col>
           <Col><label><input type="radio" name="sweetS" value="spk" onChange={this.handleOptionChange}/><img className="Selection-box" src={SourPatchKids}/></label></Col>
@@ -107,8 +120,8 @@ class App extends React.Component {
           <Col><label><input type="radio" name="saltyS" value="pr" onChange={this.handleOptionChange}/><img className="Selection-box" src={Pretzels}/></label></Col>
           <Col><label><input type="radio" name="saltyS" value="ch" onChange={this.handleOptionChange}/><img className="Selection-box" src={Chips}/></label></Col>
           <Col><label><input type="radio" name="saltyS" value="pe" onChange={this.handleOptionChange}/><img className="Selection-box" src={Peanuts}/></label></Col>
-        </Row>
-        <p>6. Name</p>
+        </Row> */}
+        <p>Name</p>
         <Row className="Selection-row">
           <input type="text" name="name" value={this.state.name} onChange={this.handleOptionChange}/>
         </Row>
